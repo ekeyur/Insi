cdc182a49078274f7934e5a2715f6b9aaf0c2506
Keyur Patel <ekeyur@gmail.com>
2017-01-31 16:23:00 -0500

    delete company working. Added more companies to make the page look balanced

--- index.js
+++ index.js
@@ -46,7 +46,11 @@ app.factory('API',function($http){
 
   service.companies = [
     {"name": "Krishna Ventures","status":"Researching","category":"Computer Software & Services","contacts":["Keyur Patel, CTO, (342) 453-5432","Jason Stathan, CEO, (123) 436-5678"],"comments": "Guns don't kill people. Chuck Norris kills people, Chuck Norris can rub one penny together, Chuck Norris doesn't go hunting... CHUCK NORRIS GOES KILLING. When Chuck Norris plays dodge ball...the balls dodge him. Chuck Norris can win a game of Connect Four in only three moves. ","performance":[{"key" : "quantity","bar":"true","values":[[2011,10000],[2012,12000],[2013,13000],[2014,15000],[2015,20000],[2016,23000]]}]},
-    {"name": "Insiten","status":"Pending","category":"M&A Assist Software","contacts":["Adam Trien, CEO, (123) 456-7890","Gentry Ganote, President & CFO, (324) 432-4312"],"comments": "Chuck Norris does not get frostbite. Chuck Norris bites frost. Contrary to popular belief, Chuck Norris, not the box jellyfish of northern Australia, is the most venomous creature on earth, What was going through the minds of all of Chuck Norris' victims before they died? His shoe, Chuck Norris counted to infinity - twice. ","performance":[{"key" : "quantity","bar":"true","values":[[2011,5000],[2012,6000],[2013,8000],[2014,10000],[2015,15000],[2016,23000]]}]}
+    {"name": "Insiten","status":"Pending","category":"M&A Assist Software","contacts":["Adam Trien, CEO, (123) 456-7890","Gentry Ganote, President & CFO, (324) 432-4312"],"comments": "Chuck Norris does not get frostbite. Chuck Norris bites frost. Contrary to popular belief, Chuck Norris, not the box jellyfish of northern Australia, is the most venomous creature on earth, What was going through the minds of all of Chuck Norris' victims before they died? His shoe, Chuck Norris counted to infinity - twice. ","performance":[{"key" : "quantity","bar":"true","values":[[2011,5000],[2012,6000],[2013,8000],[2014,10000],[2015,15000],[2016,23000]]}]},
+    {"name": "Facebook","status":"Approved","category":"Social Media","contacts":["Adam Trien, CEO, (123) 456-7890","Gentry Ganote, President & CFO, (324) 432-4312"],"comments": "The leading causes of death in the United States are: 1. Heart Disease 2. Chuck Norris 3. Cancer, If you spell Chuck Norris in Scrabble, you win. Forever, What was going through the minds of all of Chuck Norris' victims before they died? His shoe The US did not boycott the 1980 Summer Olympics in Moscow due to political reasons: Chuck Norris killed the entire US team with a single round-house kick during TaeKwonDo practice.","performance":[{"key" : "quantity","bar":"true","values":[[2011,1000],[2012,3000],[2013,8000],[2014,5000],[2015,7000],[2016,4000]]}]},
+    {"name": "Intuit","status":"Pending","category":"Business Software","contacts":["Adam Trien, CEO, (123) 456-7890","Gentry Ganote, President & CFO, (324) 432-4312"],"comments": "Chuck Norris does not get frostbite. Chuck Norris bites frost. Contrary to popular belief, Chuck Norris, not the box jellyfish of northern Australia, is the most venomous creature on earth, What was going through the minds of all of Chuck Norris' victims before they died? His shoe, Chuck Norris counted to infinity - twice. ","performance":[{"key" : "quantity","bar":"true","values":[[2011,5000],[2012,4000],[2013,6000],[2014,7000],[2015,15000],[2016,18000]]}]},
+    {"name": "Amazon","status":"Researching","category":"Internet Retailer","contacts":["Adam Trien, CEO, (123) 456-7890","Gentry Ganote, President & CFO, (324) 432-4312"],"comments": "Chuck Norris does not get frostbite. Chuck Norris bites frost. Contrary to popular belief, Chuck Norris, not the box jellyfish of northern Australia, is the most venomous creature on earth, What was going through the minds of all of Chuck Norris' victims before they died? His shoe, Chuck Norris counted to infinity - twice. ","performance":[{"key" : "quantity","bar":"true","values":[[2011,5000],[2012,6000],[2013,8000],[2014,10000],[2015,15000],[2016,23000]]}]},
+    {"name": "Blackberry","status":"Pending","category":"Mobile Phones","contacts":["Adam Trien, CEO, (123) 456-7890","Gentry Ganote, President & CFO, (324) 432-4312"],"comments": "Chuck Norris does not get frostbite. Chuck Norris bites frost. Contrary to popular belief, Chuck Norris, not the box jellyfish of northern Australia, is the most venomous creature on earth, What was going through the minds of all of Chuck Norris' victims before they died? His shoe, Chuck Norris counted to infinity - twice. ","performance":[{"key" : "quantity","bar":"true","values":[[2011,20000],[2012,17000],[2013,12000],[2014,8000],[2015,7000],[2016,8000]]}]},
   ];
   return service;
 });
@@ -54,23 +58,18 @@ app.factory('API',function($http){
 //Controller that shows the list of all the companies.
 // Deleting and editing a company functions are also present here.
 app.controller('companiesController',function($scope,API,$state,$rootScope){
-  // API.companies().success(function(data){
-  //   $scope.companies = data;
-  //   $scope.delete = function($index){
-  //     $scope.companies.splice($index,1);
-  //   };
-  // });
-  $rootScope.editC = false;
+
+
   $scope.companies = API.companies;
 
-  $scope.delete = function($index){
-    $scope.companies.splice($index,1);
+  //Function to delete a record
+  $scope.delete = function(name){
+    $scope.companies = $scope.companies.filter(function(item){
+      return item.name !== name;
+    });
+    $state.go('companies');
   };
 
-  // $scope.edit = function($index){
-  //   $scope.editcompany = API.companies[$index];
-  //   $rootScope.editC = true;
-  // };
   });
 
 
@@ -83,6 +82,7 @@ $scope.addcompany = function(){
     "status" : $scope.status,
     "contacts" : [$scope.contact1,$scope.contact2],
     "comments" : $scope.comments
+    
     };
   API.companies.push(newcompany);
   $state.go('companies');
@@ -94,7 +94,6 @@ app.controller('companyController',function($scope,API,$stateParams,$rootScope){
     let cname = API.companies.filter(function(a){
       return a.name === $stateParams.name;
     });
-    $rootScope.editC = false;
     // Display Graph
     let options = {
       chart: {
@@ -128,11 +127,6 @@ app.controller('companyController',function($scope,API,$stateParams,$rootScope){
                         return d3.format(',.0f')(d);
                     }
                 },
-                // tooltip: {
-                //     keyFormatter: function(d) {
-                //         return d3.time.format('%x')(new Date(d));
-                //     }
-                // },
                 zoom: {
                     enabled: true,
                     scaleExtent: [1, 10],
@@ -144,8 +138,8 @@ app.controller('companyController',function($scope,API,$stateParams,$rootScope){
                 }
             }
         };
+        // Add options to the object
         cname[0].options = options;
-        console.log("company object after adding the options",cname[0]);
         $scope.companyInfo = cname[0];
   });
 
