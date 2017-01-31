c320475191ae6ba636c9037ced7993571ca7c988
Keyur Patel <ekeyur@gmail.com>
2017-01-30 23:49:25 -0500

    Added chart, moved delete and edit button to company info div

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = angular.module('insiten',['ui.router','nvd3']);
 
-app.config(function($stateProvider,$urlRouterProvider){
+app.config(function($stateProvider,$urlRouterProvider,$locationProvider){
+  $locationProvider.hashPrefix('');
   $stateProvider
   .state({
     name: 'companies',
@@ -44,8 +45,8 @@ app.factory('API',function($http){
   // };
 
   service.companies = [
-    {"name": "Rigor","status":"researching","category":"Computer Software & Services","contacts":["Keyur Patel","Jason Stathan"],"comments": "poor","performance":[[2011,10000],[2012,12000],[2013,13000],[2014,15000],[2015,20000],[2016,23000]]},
-    {"name": "Insiten","status":"pending","category":"M&A assist Software","contacts":["Adam Trien","Gentry Ganote "],"comments": "good","performance":[[2011,10000],[2012,12000],[2013,15000],[2014,20000],[2015,28000],[2016,39000]]}
+    {"name": "Krishna Ventures","status":"Researching","category":"Computer Software & Services","contacts":["Keyur Patel, CTO, (342) 453-5432","Jason Stathan, CEO, (123) 436-5678"],"comments": "Guns don't kill people. Chuck Norris kills people, Chuck Norris can rub one penny together, Chuck Norris doesn't go hunting... CHUCK NORRIS GOES KILLING. When Chuck Norris plays dodge ball...the balls dodge him. Chuck Norris can win a game of Connect Four in only three moves. ","performance":[{"key" : "quantity","bar":"true","values":[[2011,10000],[2012,12000],[2013,13000],[2014,15000],[2015,20000],[2016,23000]]}]},
+    {"name": "Insiten","status":"Pending","category":"M&A Assist Software","contacts":["Adam Trien, CEO, (123) 456-7890","Gentry Ganote, President & CFO, (324) 432-4312"],"comments": "Chuck Norris does not get frostbite. Chuck Norris bites frost. Contrary to popular belief, Chuck Norris, not the box jellyfish of northern Australia, is the most venomous creature on earth, What was going through the minds of all of Chuck Norris' victims before they died? His shoe, Chuck Norris counted to infinity - twice. ","performance":[{"key" : "quantity","bar":"true","values":[[2011,5000],[2012,6000],[2013,8000],[2014,10000],[2015,15000],[2016,23000]]}]}
   ];
   return service;
 });
@@ -95,49 +96,48 @@ app.controller('companyController',function($scope,API,$stateParams,$rootScope){
     });
     $rootScope.editC = false;
     // Display Graph
-
-    var options = {
-            chart: {
+    let options = {
+      chart: {
                 type: 'historicalBarChart',
-                height: 450,
+                height: 320,
                 margin : {
                     top: 20,
                     right: 20,
-                    bottom: 65,
+                    bottom: 60,
                     left: 50
                 },
                 x: function(d){return d[0];},
-                y: function(d){return d[1]/100000;},
+                y: function(d){return d[1];},
                 showValues: true,
                 valueFormat: function(d){
                     return d3.format(',.1f')(d);
                 },
                 duration: 100,
                 xAxis: {
-                    axisLabel: 'X Axis',
+                    axisLabel: 'Year Ending',
                     tickFormat: function(d) {
-                        return d3.time.format('%x')(new Date(d))
+                        return d3.format('.0f')(d);
                     },
-                    rotateLabels: 30,
-                    showMaxMin: false
+                    rotateLabels: 50,
+                    showMaxMin: true
                 },
                 yAxis: {
-                    axisLabel: 'Y Axis',
+                    axisLabel: 'Revenue ($)',
                     axisLabelDistance: -10,
                     tickFormat: function(d){
-                        return d3.format(',.1f')(d);
+                        return d3.format(',.0f')(d);
                     }
                 },
-                tooltip: {
-                    keyFormatter: function(d) {
-                        return d3.time.format('%x')(new Date(d));
-                    }
-                },
+                // tooltip: {
+                //     keyFormatter: function(d) {
+                //         return d3.time.format('%x')(new Date(d));
+                //     }
+                // },
                 zoom: {
                     enabled: true,
                     scaleExtent: [1, 10],
                     useFixedDomain: false,
-                    useNiceScale: false,
+                    useNiceScale: true,
                     horizontalOff: false,
                     verticalOff: true,
                     unzoomEventType: 'dblclick.zoom'
@@ -145,7 +145,7 @@ app.controller('companyController',function($scope,API,$stateParams,$rootScope){
             }
         };
         cname[0].options = options;
-        console.log("optoinasfasd asdf",cname[0]);
+        console.log("company object after adding the options",cname[0]);
         $scope.companyInfo = cname[0];
   });
 
